6b0b05e778acba43fea91ffe692bd4256a787257
Hi-tech <Hi-tech@DESKTOP-K8PC2B7>
2024-01-31 19:09:26 +0200

    Добавьте файлы проекта.

---
+++ DateClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class DateClass
+    {
+        private DateTime data;
+
+        // конструктор без параметров
+        public DateClass()
+        {
+            data = DateTime.Now;
+        }
+
+        // конструктор с  параметрами 
+        public DateClass(int year, int month, int day)
+        {
+            SetDate(year, month, day);
+        }
+
+        // свойство для доступа к полю класса
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        // метод для установки  даты
+        public void SetDate(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+
+        // метод для вычисления даты пред. дня
+        public DateTime PreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        // метод для вычисления даты след. дня
+        public DateTime NextDay()
+        {
+            return data.AddDays(1);
+        }
+
+        // Метод для определения, сколько дней осталось до конца месяца
+        public int DaysLeft()
+        {
+            DateTime lastDayOfMonth = new DateTime(data.Year, data.Month, DateTime.DaysInMonth(data.Year, data.Month));
+            return (lastDayOfMonth - data).Days;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите месяц: ");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите день: ");
+            int day = int.Parse(Console.ReadLine());
+
+            DateClass dateObject = new DateClass(year, month, day);
+
+            Console.WriteLine($"Текущая дата: {dateObject.Data}");
+            Console.WriteLine($"Предыдущий день: {dateObject.PreviousDay()}");
+            Console.WriteLine($"Следующий день: {dateObject.NextDay()}");
+            Console.WriteLine($"Дней до конца месяца: {dateObject.DaysLeft()}");
+            Console.ReadKey();
+        }
+    }
+
+}
+    
